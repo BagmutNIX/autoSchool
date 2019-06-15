d4a9723ed2e4bb5aa4af1ab73dbf66fd10c22233
dove <inna.bahmut@nixsolutions.com>
2019-06-15 19:54:24 +0300

    First commit

--- scr/main/java/HelloWorld.java
+++ scr/main/java/HelloWorld.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
+
 public class HelloWorld {
+
+    public static void printHelloName() {
+        System.out.println("What is your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(String.format("Hello %s", name));
+    }
+
+
+
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+        printHelloName();
     }
 }
+
---
+++ scr/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+        {
+public static void main(String[]args){
+
+        List<String> one=new ArrayList<>();
+        one.add("first");
+        List<String> two=new ArrayList<>();
+        //two=one;
+        two.add("second");
+        int i=0;
+
+            System.out.println(one);
+        }
+        }
