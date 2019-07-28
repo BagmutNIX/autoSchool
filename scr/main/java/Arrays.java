7e1daf1e815aa033ef39050bf45feba2eb6b19bc
BagmutNIX <energy777&>
2019-07-28 22:20:05 +0300

    21

--- scr/main/java/Arrays.java
+++ scr/main/java/Arrays.java
@@ -1,6 +1,6 @@
 public class Arrays {
     public static void example() {
-        int[] numbers = new int[10];
+        int[] numbers = new int[21];
         //int output = 0;
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
