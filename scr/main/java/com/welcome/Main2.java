71ca59851a28cc6139f949b6b771c9a35bd27f77
dove <inna.bahmut@nixsolutions.com>
2019-08-06 20:14:40 +0300

    conditions

--- scr/main/java/com/welcome/Main2.java
+++ scr/main/java/com/welcome/Main2.java
@@ -2,18 +2,21 @@ package com.welcome;
 
 import com.welcome.Hello;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
        /*     System.out.println("What is your name? ");
             Scanner scanner = new Scanner(System.in);
             String name = scanner.nextLine();
             System.out.println(String.format("Hello %s", name));*/
         //Hello you = new Hello();
+        //Hello.printHelloName();
 
-        Hello.printHelloName();
+        Conditions_Cycles.minimum();
     }
 }
+
 
