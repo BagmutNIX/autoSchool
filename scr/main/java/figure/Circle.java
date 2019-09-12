b2e4c44664cb1a3eaabcc228d541ff3a585959aa
dove <inna.bahmut@nixsolutions.com>
2019-09-12 21:11:55 +0300

    figures - beginning

--- scr/main/java/figure/Circle.java
+++ scr/main/java/figure/Circle.java
@@ -1,5 +1,9 @@
 package figure;
 
 public class Circle {
-    
+    public static void cSquare() {
+        double size = 16.78;
+        double square = 3.14 * size * size;
+        System.out.println(square);
+    }
 }
