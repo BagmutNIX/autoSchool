94fbfd9c5f7a9fcca8a7b20fe86c6cf396b208e1
dove <inna.bahmut@nixsolutions.com>
2019-09-15 17:33:47 +0300

    figures - beginning

--- scr/main/java/figure/Circle.java
+++ scr/main/java/figure/Circle.java
@@ -1,9 +1,26 @@
 package figure;
 
-public class Circle extends Figure{
-    public static void cSquare() {
-        double size = 16.78;
-        double square = 3.14 * size * size;
-        System.out.println(square);
+public class Circle extends Figure {
+    //конструктор нужен для инициализации объекта/класса
+    private double radius = 0;
+    private String objectName = "Circle";
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        //double size = 16.78;
+        //double square = 3.14 * size * size;
+        // System.out.println(square);
+        return Math.PI * radius * radius;
+    }
+
+    public String name() {
+        return objectName;
+    }
+
+    public double size() {
+        return radius;
     }
 }
--- scr/main/java/figure/Figure.java
+++ scr/main/java/figure/Figure.java
@@ -1,5 +1,7 @@
 package figure;
 
 public abstract class Figure {
-    
+    public abstract double area();
+    public abstract String name();
+    public abstract double size();
 }
