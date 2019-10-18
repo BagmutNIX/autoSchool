2f05f78cd7aff31dc71ab4ad5315f2168a5a8eae
dove <inna.bahmut@nixsolutions.com>
2019-10-18 14:15:05 +0300

    collections

--- scr/main/java/generics/collections/stream/Collections.java
+++ scr/main/java/generics/collections/stream/Collections.java
@@ -1,10 +1,15 @@
 package generics.collections.stream;
 
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+/*1. HashSet из растений
+        Создать коллекцию HashSet с типом элементов String.
+        Добавить в неё 10 строк: арбуз, банан, вишня, груша, дыня, ежевика, жень-шень, земляника, ирис, картофель.
+        Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+        Посмотреть, как изменился порядок добавленных элементов.*/
 
 public class Collections {
     public static void hashSetCollection() throws IOException {
@@ -17,6 +22,7 @@ public class Collections {
         set.add("Blackberry");
         set.add("Ginseng");
         set.add("strawberry");
+        set.add("Iris");
         set.add("potato");
         Iterator<java.lang.String> iterator = set.iterator();
         while (iterator.hasNext()) {
@@ -24,4 +30,37 @@ public class Collections {
             System.out.println(text);
         }
     }
+/*    2. HashMap из 10 пар
+    Создать коллекцию HashMap<String, String>, занести туда 10 пар строк: арбуз – ягода, банан – трава, вишня – ягода, груша – фрукт, дыня – овощ, ежевика – куст, жень-шень – корень, земляника – ягода, ирис – цветок, картофель – клубень.
+    Вывести содержимое коллекции на экран, каждый элемент с новой строки.
+            Пример вывода (тут показана только одна строка):
+    картофель – клубень*/
+    public static void hashMapCollection() {
+        Map<java.lang.String, java.lang.String> map = new HashMap<>();
+        map.put("Watermelon", "berry");
+        map.put("Banana", "grass");
+        map.put("Cherry", "berry");
+        map.put("Pear", "fruit");
+        map.put("Cucumber", "vegetable;");
+        map.put("Blackberry", "bush");
+        map.put("Ginseng", "root");
+        map.put("Strawberry", "berry");
+        map.put("Iris", "flower");
+        map.put("Potato", "tuber");
+        Iterator<Map.Entry<java.lang.String, java.lang.String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext())
+        {
+            Map.Entry<java.lang.String, java.lang.String> pair = iterator.next();
+            java.lang.String key = pair.getKey();
+            java.lang.String value = pair.getValue();
+            System.out.println(key + " - " + value);
+        }
+    }
+
+/*    3. Коллекция HashMap из котов
+    Есть класс Cat, с полем имя (name, String).
+    Создать коллекцию HashMap<String, Cat>.
+    Добавить в коллекцию 10 котов, в качестве ключа использовать имя кота.
+    Вывести результат на экран, каждый элемент с новой строки.*/
+
 }
--- scr/main/java/generics/collections/stream/Main.java
+++ scr/main/java/generics/collections/stream/Main.java
@@ -2,6 +2,7 @@ package generics.collections.stream;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        Collections.hashSetCollection();
+        //Collections.hashSetCollection();
+        Collections.hashMapCollection();
     }
 }
